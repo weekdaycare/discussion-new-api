d00acff8c83828aec2a476f1b23dddbed1ef96de
weekdaycare <xiabee@duck.com>
2024-12-24 19:54:55 +0800

    [feat] push repo to Marketplace

--- get_comments/config.py
+++ get_comments/config.py
@@ -1,8 +1,16 @@
-# -*- coding: utf-8 -*-
-import yaml
+import os
 
 def load():
-    f = open('config.yml', 'r', encoding='utf-8')
-    ystr = f.read()
-    ymllist = yaml.load(ystr, Loader=yaml.FullLoader)
-    return ymllist
+    return {
+        'issue': {
+            'enable': os.getenv('ISSUE_ENABLE', 'true').lower() == 'true',
+            'github_user', 'github_repo' = os.getenv('GITHUB_REPO').split('/')
+            'limit': int(os.getenv('LIMIT', 10))
+        },
+        'discussion': {
+            'enable': os.getenv('DISCUSSION_ENABLE', 'true').lower() == 'true',
+            'github_user', 'github_repo' = os.getenv('GITHUB_REPO').split('/')
+            'category_id': os.getenv('CATEGORY_ID'),
+            'limit': int(os.getenv('LIMIT', 10))
+        }
+    }
